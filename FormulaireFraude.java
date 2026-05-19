c052760a9ad2d4ec10019e165915228ed8478b03
E-ytram <egide.marty25@gmail.com>
2026-05-19 16:34:07 +0200

    debut formulaire Fraude

--- FormulaireFraude.java
+++ FormulaireFraude.java
@@ -1,16 +1,70 @@
+import fraude.Fraude;
 import java.time.LocalDateTime;
 import java.util.List;
 
 class FormulaireFraude {
-    int id;
-    LocalDateTime dateCreation ;
-    LocalDateTime dateDerniereModification;
-    Epreuve epreuve;
-    List<Etudiant> etudiants;
-    List<Fraude> fraudes;
+    private int id;
+    private LocalDateTime dateCreation ;
+    private LocalDateTime dateDerniereModification;
+    private Epreuve epreuve;
+    private List<Etudiant> etudiants;
+    private List<Fraude> fraudes;
 
     void ajouterEtudiant(Etudiant etudiant){
-
+        etudiants.add(etudiant);
     }
-    ajouterFraude(Fraude fraude) : void
+    void ajouterFraude(Fraude fraude){
+        fraudes.add(fraude);
+        dateDerniereModification = LocalDateTime.now();
+    }
+
+
+
+    public List<Fraude> getFraudes() {
+        return fraudes;
+    }
+
+    public void setFraudes(List<Fraude> fraudes) {
+        this.fraudes = fraudes;
+    }
+
+    public List<Etudiant> getEtudiants() {
+        return etudiants;
+    }
+
+    public void setEtudiants(List<Etudiant> etudiants) {
+        this.etudiants = etudiants;
+    }
+
+    public Epreuve getEpreuve() {
+        return epreuve;
+    }
+
+    public void setEpreuve(Epreuve epreuve) {
+        this.epreuve = epreuve;
+    }
+
+    public LocalDateTime getDateDerniereModification() {
+        return dateDerniereModification;
+    }
+
+    public void setDateDerniereModification(LocalDateTime dateDerniereModification) {
+        this.dateDerniereModification = dateDerniereModification;
+    }
+
+    public LocalDateTime getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(LocalDateTime dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
